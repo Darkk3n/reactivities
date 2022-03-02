539835b1dcf79282432267dadad8f15f9ca706cf
Gerardo <gerardo.aguilar01@outlook.com>
2022-03-02 16:53:00 -0600

    Walking Skeleton part 1

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,32 @@
+using System;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+   public class ActivitiesController : BaseApiController
+   {
+      private readonly DataContext context;
+
+      public ActivitiesController(DataContext context)
+      {
+         this.context = context;
+      }
+
+      [HttpGet]
+      public async Task<ActionResult<List<Activity>>> GetActivities()
+      {
+         return await context.Activities.ToListAsync();
+      }
+
+      [HttpGet("{id}")]
+      public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+      {
+         return await context.Activities.FindAsync(id);
+      }
+   }
+}
